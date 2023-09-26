29e05964da69274754903f8fc13755b37dd98091
hritik pandey <128885231+hri1234@users.noreply.github.com>
2023-09-26 17:09:49 +0530

    Update database.js
    1st commit

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI ='mongodb+srv://hrithik:io9nn9UObqOcARiI@cluster0.fe4kclq.mongodb.net/ecomm';
 
 const connectDatabase = () => {
     mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
